651093763b6ef083ace1a2fa00a6effa6b43f60f
MArcio Furukawa <ykz.furukawa@gmail.com>
2022-02-21 00:27:27 -0300

    Remoçao de arquivo teste, atualizaçao de gitignore.

--- teste.py
+++
@@ -1,9 +0,0 @@
-# %%
-import boto3
-
-# %%
-# teste criado para verificar se as credenciais AWS foram criadas corretamente na máquina
-s3 = boto3.resource('s3')
-s3.create_bucket(Bucket='bucket-via-boto3')
-
-# %%
